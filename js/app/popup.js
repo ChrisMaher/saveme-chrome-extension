5f35e40ab896b3af5d4cbc17c0516ccad308c1da
Chris Maher <chrismaher.wit@gmail.com>
2016-03-21 02:48:36 +0000

    Chrome Extension First Commit.

---
+++ js/app/popup.js
@@ -0,0 +1,20 @@
+﻿myApp.controller("PageController", function ($scope, $http) {
+
+
+    $http.get('http://www.saveme.ie/api/savings').success(function (data) {
+        $scope.savings = data;
+
+        for (i = 0; i < $scope.savings.length; i++) {
+            if ($scope.savings[i].title.length > 32) {
+                $scope.savings[i].title = $scope.savings[i].title.slice(0,32);
+            }
+
+            $scope.savings[i].title = $scope.savings[i].title.toUpperCase();
+        }
+
+    });
+
+});
+
+
+
